0524ae4ace1410e3dfe2c210467edfdc3c3b7e36
Artaius <36020251+xartaiusx@users.noreply.github.com>
2025-10-12 04:59:35 -0700

    Update astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://shotsofrhapsody.com//",
+	site: "https://shots-of-rhapsody.github.io/Shots-of-Rhapsody/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
